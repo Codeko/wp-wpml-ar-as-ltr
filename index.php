b87ee5228ebe434d2753df1b913f1509af1b88d3
Pablo Hinojosa <phinojosa@codeko.com>
2025-07-31 10:10:02 +0200

    fix: updater file not included if class yet exist

--- index.php
+++ index.php
@@ -14,7 +14,9 @@ if (!defined('ABSPATH')) {
 add_action( 'init', 'wp_wpml_ar_as_ltr_github_plugin_updater' );
 
 function wp_wpml_ar_as_ltr_github_plugin_updater() {
-    include_once('updater.php');
+    if (!class_exists( 'WP_GitHub_Updater' ) ){
+        include_once('updater.php');
+    }
     define( 'WP_GITHUB_FORCE_UPDATE', true );
 
     if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
